d37ee557755c1eb898223469e8193a54a875f62b
Faustin Kovungbo <faustinkov17@gmail.com>
2024-05-23 10:28:18 +0100

    "Updated slider functionality: changed querySelectorAll to querySelector, added event listeners for prev and next buttons, and modified changeSlide function"

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 const imgs = document.querySelectorAll('.header-slider ul img')
-const prev_btn = document.querySelectorAll('.control_preview')
-const next_btn = document.querySelectorAll('.control_next')
+const prev_btn = document.querySelector('.control_preview')
+const next_btn = document.querySelector('.control_next')
 
-let n =0;
+let n = 0 ;
 
 function changeSlide(){
-     for(let i=0; i<imgs.length; i++){
+     for(let i=0; i < imgs.length; i++){
         imgs[i].style.display='none'
      }
+     imgs[n].style.display='block'
 }
 changeSlide()
+prev_btn.addEventListener('click', (e)=>{
+    if (n > 0 ) {
+        n--;
+    }
+    else {
+        n = imgs.length-1;
+    }
+    changeSlide();
+})
+
+next_btn.addEventListener('click', (e)=>{
+    if (n < imgs.length-1) {
+        n++;
+    }
+    else {
+        n = 0;
+    }
+    changeSlide();
+})
